4ac459fa836a96493b8f4a372a75572c029ab397
dalquier <alquierdamien@yahoo.com>
2026-07-26 17:33:06 +0200

    Add V6.2 config

---
+++ Scriptable/GitHub_Content_Manager_V6_2_20260726_173000/projet/config.py
@@ -0,0 +1,9 @@
+APP_NAME = "GitHub Content Manager V6.2"
+DEFAULT_SETTINGS = {
+    "repository": "dalquier/Scriptable",
+    "branch": "main",
+    "root_path": "Scriptable",
+    "token": "",
+}
+LOCAL_SETTINGS_FILENAME = "github_manager_v6_2.local.json"
+DISPLAY_MODE_FALLBACK = 1
