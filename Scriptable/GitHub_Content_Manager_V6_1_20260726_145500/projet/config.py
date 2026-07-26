71c1dccf498d26792143f0eb3063a14e28c57002
dalquier <alquierdamien@yahoo.com>
2026-07-26 16:51:41 +0200

    Add V6.1 default configuration

---
+++ Scriptable/GitHub_Content_Manager_V6_1_20260726_145500/projet/config.py
@@ -0,0 +1,10 @@
+APP_NAME = "GitHub Content Manager V6.1"
+APP_VERSION = "6.1.0"
+
+DEFAULT_REPOSITORY = "dalquier/Scriptable"
+DEFAULT_BRANCH = "main"
+DEFAULT_ROOT_PATH = "Scriptable"
+DEFAULT_TOKEN = ""
+
+# Pyto display mode. Use SHEET to avoid fullscreen.
+DISPLAY_MODE = "sheet"
